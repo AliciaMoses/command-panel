12dedc590ded36a12f240b9ffa63c85a85cd7b47
Alicia Moses <aliciamoses.dev@gmail.com>
2023-05-26 23:55:05 +0100

    Add script class to models

--- commandpanel_project/commandpanel/models.py
+++ commandpanel_project/commandpanel/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+class Script(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    code = models.TextField()
