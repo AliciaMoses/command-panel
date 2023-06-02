cee9725226711e6baf10cef97a0a287c23e2d950
Alicia Moses <aliciamoses.dev@gmail.com>
2023-06-02 15:04:25 +0100

    Passing unit test for ScriptModel

--- commandpanel_project/commandpanel/tests.py
+++ commandpanel_project/commandpanel/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Script
+
+class ScriptModelTest(TestCase):
+    
+    def setUp(self):
+        self.script = Script.objects.create(
+            name='Test Script',
+            description='This is a test script',
+            code='print("Hello, world!")'
+        )
+
+    def test_script_creation(self):
+        self.assertEqual(f'{self.script.name}', 'Test Script')
+        self.assertEqual(f'{self.script.description}', 'This is a test script')
+        self.assertEqual(f'{self.script.code}', 'print("Hello, world!")')
+
