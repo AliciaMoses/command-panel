9eab16133376193cd94d068d3b46dda501c03179
Alicia Moses <aliciamoses.dev@gmail.com>
2023-06-03 22:37:36 +0100

    Passing test for Parameter Model

--- commandpanel_project/commandpanel/tests.py
+++ commandpanel_project/commandpanel/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Script
+from .models import Script, Parameter
 
 class ScriptModelTest(TestCase):
     
@@ -15,4 +15,24 @@ class ScriptModelTest(TestCase):
         self.assertEqual(f'{self.script.name}', 'Test Script')
         self.assertEqual(f'{self.script.description}', 'This is a test script')
         self.assertEqual(f'{self.script.code}', 'print("Hello, world!")')
+        
+class ParameterModelTest(TestCase):
+    
+    def setUp(self):
+        self.script = Script.objects.create(
+            name='Test Script',
+            description='This is a test script',
+            code='print("Hello, {}!")'
+        )
+        
+        self.parameter = Parameter.objects.create(
+            script=self.script,
+            name='name',
+            value='world'
+        )
+
+    def test_parameter_creation(self):
+        self.assertEqual(f'{self.parameter.name}', 'name')
+        self.assertEqual(f'{self.parameter.value}', 'world')
+        self.assertEqual(self.parameter.script, self.script)
 
